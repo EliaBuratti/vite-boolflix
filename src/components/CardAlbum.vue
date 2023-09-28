4875a080b9e4056d5eaf01caa26f5af3d9910219
EliaBuratti <elia.buratti.dev@gmail.com>
2023-09-28 17:46:16 +0200

    create card style

---
+++ src/components/CardAlbum.vue
@@ -0,0 +1,26 @@
+<script>
+import { state } from '../assets/dataJs/state';
+import Card from './Card.vue';
+
+export default {
+    name: 'CardAlbum',
+
+    components: {
+        Card,
+    },
+
+    data() {
+        return {
+            state,
+        }
+    },
+
+
+}
+</script>
+<template>
+    <Card v-for="film in state.listMovies" :film="film" />
+</template>
+
+
+<style lang="scss" scoped></style>
